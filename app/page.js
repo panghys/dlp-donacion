55cca414fd9ec225c18f13bd0e433a2f59525ae0
panghys <rbgm248@gmail.com>
2024-10-08 12:36:46 -0400

    Creación de qrs en base a la id del libro

--- app/page.js
+++ app/page.js
@@ -1,25 +1,30 @@
 "use client";
-import {  persona,libro } from "./funciones/datos.js";
-//Donar Libros
+import { useRouter } from "next/navigation";
+import { persona, libro } from "./funciones/datos.js";
+
+// Donar Libros
 export default function Home() {
+  const router = useRouter();
+  const handleGuardarClick = () => {
+    router.push("/formulario/qr");
+  };
   return (
-  <div>
-    <h1 className="titulo">Donar Libros</h1>
-    <div className="cuestionario">
-      <h1 className="titulo1">Informacion del Usuario</h1>
-      <div>{persona()}</div>
+    <div>
+      <h1 className="titulo">Donar Libros</h1>
+      <div className="cuestionario">
+        <h1 className="titulo1">Información del Usuario</h1>
+        <div>{persona()}</div>
+      </div>
+      <div className="cuestionario">
+        <h1 className="titulo1">Información del Libro</h1>
+        <div>{libro()}</div>
+      </div>
+      <div className="footer">
+        <button className="btn">Atrás</button>
+        <button className="btn" onClick={handleGuardarClick}>
+          Guardar Información
+        </button>
+      </div>
     </div>
-
-    <div className="cuestionario">
-      <h1 className="titulo1">Informacion del Libro</h1>
-      <div>{libro()}</div>
-        
-    </div>
-
-    <div className='footer'>
-      <button className='btn'>Atras</button>
-      <button className='btn'>Guardar</button>
-    </div>
-  </div>
-  )
+  );
 }
