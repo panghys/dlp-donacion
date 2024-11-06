a52ff1ae3a829e1e21de034cfc0cdb4e58f6015c
Erick-ALG <erick.angelito001@gmail.com>
2024-11-06 02:26:52 -0300

    page.js arreglado

--- app/page.js
+++ app/page.js
@@ -5,34 +5,40 @@ import { persona, codigo } from "./funciones/datos.js";
 export default function Home() {
   const router = useRouter();
   const handleGuardarClick = () => {
-    const correoInput = document.querySelector('input[name="correo"]');
-    const correo = correoInput ? correoInput.value : '';
+    
+    const correo = document.querySelector('input[name="correo"]')?.value || '';
+    const tituloLibro = document.querySelector('input[placeholder="Escriba el nombre correctamente"]')?.value || '';
+    const autores = document.querySelector('input[placeholder="si es mas de uno separelo por comas"]')?.value || '';
+    const generos = Array.from(document.querySelectorAll('input[name="Genero"]:checked')).map(e => e.value);
+  
     const datosLibro = {
-      titulo: document.querySelector('input[placeholder="Escriba el nombre correctamente"]').value,
-      autores: document.querySelector('input[placeholder="si es mas de uno separelo por comas"]').value,
-      tags: Array.from(document.querySelectorAll('input[name="Genero"]:checked')).map(e => e.value),
+      titulo: tituloLibro,
+      autores: autores,
+      tags: generos,
       donante: correo,
       prestado: false,
-      borrado: false,
+      borrado: false
     };
+  
+   
     const datosCompletos = {
       libro: datosLibro,
     };
+  
     const datosJSON = JSON.stringify(datosCompletos, null, 2);
     const blob = new Blob([datosJSON], { type: 'application/json' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'datos_donacion.json';  
+    link.download = 'datos_donacion.json';
     link.click();
-    URL.revokeObjectURL(link.href); 
-
+    URL.revokeObjectURL(link.href);
     router.push("/formulario/qr");
-  }
+  };
 
 return (
   <div >
     <h1 className="titulo">Donar Libros</h1>
-    
+
     <div className="cuestionario">
       <h1 className="titulo1">Informacion del Usuario</h1>
       <div>{persona()}</div>
@@ -52,4 +58,3 @@ return (
   </div>
   )
 }
-
