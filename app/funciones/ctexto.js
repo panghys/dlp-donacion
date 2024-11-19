ff38544d58ce93f7537eb9e1c9036c2b3db32cf9
DiegoTuna <diegomora329o@gmail.com>
2024-11-19 01:21:28 -0300

    ctexto.js editado

--- app/funciones/ctexto.js
+++ app/funciones/ctexto.js
@@ -8,6 +8,7 @@ const Cuadro=({dato})=>{
             value={nombre}
             placeholder={dato}
             onChange={(e) => setNombre(e.target.value)}
+            class="appearance-none rounded border-4 border-customBlue bg-primary-gradient w-72 md:w-1/3 placeholder-customGray"
         />
     );
 }
