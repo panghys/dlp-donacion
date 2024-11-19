88f6003d8a0cc999b724d0289f0b7de003f69432
DiegoTuna <diegomora329o@gmail.com>
2024-11-19 01:20:50 -0300

    datos_libro.js editado

--- app/funciones/datos _libro.js
+++ app/funciones/datos _libro.js
@@ -5,23 +5,28 @@ import Libro from "@/app/formulario/libro.json";
 
 export function no_ISBN(){
     return(
-        <div>
-            <div className='titulo1'> Debe rellenar manualmente los datos del Libro</div>
+        <div className="opacity-60">
+            <div className='text-customBlue font-sans mb-4'> Debe rellenar manualmente los datos del Libro</div>
             {Libro.cuadrotx.map((preg,j)=>(
-            <div className='rellenar'>
-                <div className='tex'>{preg.id}</div>
+            <div className='mb-6'>
+                <div className='text-lg text-white font-sans'>{preg.id}</div>
                 <Cuadro dato={preg.gris}></Cuadro>
             </div>
             ))}
             {Libro.tes.map((pregunta, i) => (
                 <div className='precionar' key={i}>
-                    <div className="titulo1">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
+                    <div className="text-lg text-white mb-4 font-sans">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
                     <div className='rellenar'>
-                        <div className='opciones'>
+                        <div className='grid grid-cols-2 md:grid-cols-3 md:mx-auto lg:mx-0 lg:grid-cols-4 gap-4 w-full md:w-3/4 text-white'>
                         {pregunta.opciones.map((opcion, j) => (
-                            <label key={j}>
-                                <input type={pregunta.tipo} name={pregunta.id} value={opcion} />
-                                {opcion}
+                            <label key={j} className="flex items-center space-x-2">
+                                <input 
+                                    type={pregunta.tipo} 
+                                    name={pregunta.id} 
+                                    value={opcion} 
+                                    class="appearance-none w-5 h-5 bg-white checked:bg-primary-gradient border-2 rounded"
+                                />
+                                <span>{opcion}</span>
                             </label>
                         ))}
                         </div>
@@ -35,23 +40,28 @@ export function no_ISBN(){
 //funcion que se vea la imagen del libro, donde se tendria que confirmar la imagen del libro o no 
 export function si_ISBN(){
     return(
-        <div>
-            <div className='titulo1'> Asegurate que los datos del libro esten todos correctos</div>
+        <div className="opacity-60">
+            <div className='text-customBlue font-sans mb-4'> Asegurate que los datos del libro esten todos correctos</div>
             {Libro.cuadrotx.map((preg,j)=>(
-            <div className='rellenar'>
-                <div className='tex'>{preg.id}</div>
+            <div className='mb-6'>
+                <div className='text-lg text-white font-sans'>{preg.id}</div>
                 <Cuadro dato={preg.gris}></Cuadro>
             </div>
             ))}
             {Libro.tes.map((pregunta, i) => (
                 <div className='precionar' key={i}>
-                    <div className="titulo1">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
+                    <div className="text-lg text-white mb-4 font-sans">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
                     <div className='rellenar'>
-                        <div className='opciones'>
+                        <div className='grid grid-cols-2 md:grid-cols-3 md:mx-auto lg:mx-0 lg:grid-cols-4 gap-4 w-full md:w-3/4 text-white'>
                         {pregunta.opciones.map((opcion, j) => (
-                            <label key={j}>
-                                <input type={pregunta.tipo} name={pregunta.id} value={opcion} />
-                                {opcion}
+                            <label key={j} className="flex items-center space-x-2">
+                                <input 
+                                    type={pregunta.tipo} 
+                                    name={pregunta.id} 
+                                    value={opcion}
+                                    class="appearance-none w-5 h-5 bg-white checked:bg-primary-gradient border-2 rounded"
+                                />
+                                <snap>{opcion}</snap>
                             </label>
                         ))}
                         </div>
