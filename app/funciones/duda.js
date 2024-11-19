7d3d75bea93947fb09f62eef8aef9c1c7aaf0738
DiegoTuna <diegomora329o@gmail.com>
2024-11-19 01:19:05 -0300

    duda.js editado

--- app/funciones/duda.js
+++ app/funciones/duda.js
@@ -29,20 +29,35 @@ const Duda = ({ isOpen, onClose }) => {
             onClose();
         }
     };
+    const handleCloseClick = () => {
+        onClose(); // Llama a la función onClose para cerrar el modal
+    };
 
     return (
-        <div className="modal-overlay" onClick={handleOverlayClick}>
-            <div className="modal-content">
-                <h2>¿Qué es el ISBN?</h2>
-                <div dangerouslySetInnerHTML={{ __html: termsText }} />
+        <div 
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" 
+            onClick={handleOverlayClick}
+        >
+            <div className="bg-white p-6 rounded-lg max-w-3xl w-full shadow-lg relative">
+                {/* Botón de cierre (X) */}
+                <button 
+                    onClick={handleCloseClick} 
+                    className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+                >
+                    &#10005;
+                </button>
+
+                <h2 className="text-2xl font-bold mb-4 text-center">¿Qué es el ISBN?</h2>
+                <div className="text-gray-700 text-sm overflow-y-auto max-h-96" dangerouslySetInnerHTML={{ __html: termsText }} />
                 <img 
-                src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6HkGGUlZna7sbOqOQuhQn9XdKLpOYmlr1kmOMT0KY0QNvtsOBQTl7gvkgK1QRmXuonz1Pg2NfBg1clljFEF5z01NwGbDQXo-dODhM1ECllzRhMbACM7Q82QZqkor0k06adj1H2r6GWWum/s1600/Qu%25C3%25A9+es+un+ISBN.png" 
-                alt="Qué es un ISBN" 
-                style={{ maxWidth: '100%', height: 'auto' }} 
-            />
+                    src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg6HkGGUlZna7sbOqOQuhQn9XdKLpOYmlr1kmOMT0KY0QNvtsOBQTl7gvkgK1QRmXuonz1Pg2NfBg1clljFEF5z01NwGbDQXo-dODhM1ECllzRhMbACM7Q82QZqkor0k06adj1H2r6GWWum/s1600/Qu%25C3%25A9+es+un+ISBN.png" 
+                    alt="Qué es un ISBN" 
+                    className="max-w-sm md:max-w-full h-auto rounded-md"
+                />
             </div>
         </div>
     );
+
 };
 
 export default Duda;
