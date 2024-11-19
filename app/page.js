5cdbf6f919d13e3697fe4c602febf4099702ed26
DiegoTuna <diegomora329o@gmail.com>
2024-11-19 01:16:15 -0300

    app/page.js editado

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { persona, codigo } from "./funciones/datos.js";
+import Image from "next/image";
 // Donar Libros
 export default function Home() {
   const router = useRouter();
@@ -34,27 +35,57 @@ export default function Home() {
     URL.revokeObjectURL(link.href);
     router.push("/formulario/qr");
   };
+  
 
+  //
 return (
-  <div >
-    <h1 className="titulo">Donar Libros</h1>
+  <div className="min-h-screen gap-20 bg-custom-gradient">
+    <header className="flex flex-col lg:flex-row gap-4 items-center border-b border-gray-400">
+      <Image
+        src="/images/logoBlanco.png"
+        alt="Logo" 
+        width={250} 
+        height={250} 
+        className="my-4 lg:mx-12 lg:my-6 bg-black bg-opacity-30 border-2 border-gray-700 rounded-[2vh]"
+      />
+      <h1 className="text-5xl font-sans text-white ">Donaciones</h1>
+      <button className="lg:absolute lg:top-8 lg:right-4 text-white">⬆️ Ir al catálogo general.</button>
+    </header>
 
-    <div className="cuestionario">
-      <h1 className="titulo1">Informacion del Usuario</h1>
-      <div>{persona()}</div>
-    </div>
-
-    <div className="cuestionario">
-      <h1 className="titulo1">Informacion del Libro</h1>
-      <div>{codigo()}</div>
-    </div>
-
-    <div className="footer">
-        <button className="btn">Atrás</button>
-        <button className="btn" onClick={handleGuardarClick}>
-          Guardar Información
-        </button>
+    <div className="flex flex-col lg:flex-row">
+      <div className="lg:w-1/3 flex items-center my-8 ">
+        <div className="hidden lg:block rounded-[2vh]">
+          <Image
+            src="/images/donaLibro.png"
+            alt="Stickman" 
+            width={300} 
+            height={300} 
+            className="rounded-lg mx-20 mb-4"
+          />
+          <p className="mx-8 text-center text-lg text-white font-sans">"Solidaridad no es dar lo que te sobra, sino dar lo que a otros les hace falta"</p>
+        </div>
       </div>
+
+      <div className="lg:w-2/3 space-y-4 p-8 text-center lg:text-left lg:overflow-y-auto lg:max-h-[calc(100vh-100px)]">
+        <div className="persona">
+          <h1 className="text-customBlue opacity-60 text-2xl font-semibold mb-4 font-sans">Informacion del Usuario</h1>
+          <div>{persona()}</div>
+        </div>
+
+        <div className="libro">
+          <h1 className="text-customBlue opacity-60 text-2xl font-semibold mb-4 font-sans">Informacion del Libro</h1>
+          <div>{codigo()}</div>
+        </div>
+
+        <div className="flex space-x-12 md:space-x-32 justify-center lg:justify-normal pt-4">
+          <button className="bg-primary-gradient text-customGray font-semibold border-4 border-customBlue w-48 h-14 rounded transition-color duration-1000 hover:bg-gradient-hover">Atrás</button>
+          <button className="bg-primary-gradient text-customGray font-semibold border-4 border-customBlue w-48 h-14 rounded transition-color duration-1000 hover:bg-gradient-hover" onClick={handleGuardarClick}>
+            Guardar Información
+          </button>
+        </div>
+
+      </div>
+    </div>
   </div>
   )
 }
