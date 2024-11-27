c9ddb8c7fc94b83b4053800375a4d54be37b870a
Javier Martinez <141840778+notos15@users.noreply.github.com>
2024-11-27 00:50:27 -0300

    Update datos _libro.js

--- app/funciones/datos _libro.js
+++ app/funciones/datos _libro.js
@@ -3,14 +3,27 @@ import React, { useState } from 'react';
 import Cuadro from "@/app/funciones/ctexto.js";
 import Libro from "@/app/formulario/libro.json";
 
-export function no_ISBN(){
+export function no_ISBN(tituloError,AutoresError){
     return(
         <div className="opacity-60">
-            <div className='text-customBlue font-sans mb-4'>Rellene manualmente los datos</div>
+            <div className='text-orange-300 font-sans mb-4 text-xl font-semibold'>Rellene manualmente los datos</div>
             {Libro.cuadrotx.map((preg,j)=>(
             <div className='mb-6'>
                 <div className='text-white font-semibold text-xl'>{preg.id}</div>
                 <Cuadro dato={preg.gris}></Cuadro>
+                {preg.gris === "Escriba el Nombre Correctamente" && tituloError && (
+                        <div className="flex items-center mt-2">
+                            <button className="bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center mr-3">!</button>
+                            <span className="text-white text-lg">Escriba el nombre del libro antes de avanzar</span>
+                        </div>
+                )}
+                {preg.gris === "Si es mas de uno separelo por comas" && AutoresError && (
+                        <div className="flex items-center mt-2">
+                            <button className="bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center mr-3">!</button>
+                            <span className="text-white text-lg">Escriba el nombre del autor antes de avanzar</span>
+                        </div>
+                )}
+
             </div>
             ))}
             {Libro.tes.map((pregunta, i) => (
@@ -43,7 +56,7 @@ export function si_ISBN(titulo,autor,imagen){
     
     return(
         <div className="">
-            <div className='text-customBlue font-sans mb-4 opacity-60'> Asegurate que los datos del libro esten todos correctos</div>
+            <div className='text-orange-300 font-sans mb-4 text-xl font-semibold'> Asegurate que los datos del libro esten todos correctos</div>
             
             <div className='opacity-60 mb-6'>
                 <div className='text-white font-semibold text-xl'>{"Nombre del Libro"}</div>
@@ -98,21 +111,33 @@ export function si_ISBN(titulo,autor,imagen){
     )
 }
 
-export function ISBN_no_funciona(){
+export function ISBN_no_funciona(tituloError,AutoresError){
     return(
         <div className="opacity-60">
-            <div className='text-orange-300 font-sans mb-4'> No se ha encontrado el libro en la libreria.
-                Rellene manualmente los datos
-            </div>
+            <div className='text-orange-300 font-sans mb-4 text-xl font-semibold'>No se ha encontrado el libro en la libreria.
+            Rellene manualmente los datos</div>
             {Libro.cuadrotx.map((preg,j)=>(
             <div className='mb-6'>
                 <div className='text-white font-semibold text-xl'>{preg.id}</div>
                 <Cuadro dato={preg.gris}></Cuadro>
+                {preg.gris === "Escriba el Nombre Correctamente" && tituloError && (
+                        <div className="flex items-center mt-2">
+                            <button className="bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center mr-3">!</button>
+                            <span className="text-white text-lg">Escriba el nombre del libro antes de avanzar</span>
+                        </div>
+                )}
+                {preg.gris === "Si es mas de uno separelo por comas" && AutoresError && (
+                        <div className="flex items-center mt-2">
+                            <button className="bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center mr-3">!</button>
+                            <span className="text-white text-lg">Escriba el nombre del autor antes de avanzar</span>
+                        </div>
+                )}
+
             </div>
             ))}
             {Libro.tes.map((pregunta, i) => (
                 <div className='precionar' key={i}>
-                    <div className='text-white font-semibold text-xl'>{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
+                    <div className='text-white font-semibold text-xl mb-6'>{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
                     <div className='rellenar'>
                         <div className='grid grid-cols-2 md:grid-cols-3 md:mx-auto lg:mx-0 lg:grid-cols-4 gap-4 w-full md:w-3/4 text-white'>
                         {pregunta.opciones.map((opcion, j) => (
